bb8943258b05d3ec94b2676223fa8288ed595adc
Your Name <you@example.com>
2022-08-02 23:42:01 +0530

    [Megha] Add: Adding Addition Part

---
+++ Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    public class Sum
+    {
+        NLog nLog = new NLog();
+
+        public void DoSum(int a,int b)
+        {
+            if(a==0 || b==0)
+            {
+                nLog.LogErroring(" A and B value Are 0");
+            }
+            else
+            {
+                int c = a + b;
+                nLog.LogDebugMode("The Program executing");
+                nLog.LogInformation("The Values Added");
+            }
+        }
+    }
+}
