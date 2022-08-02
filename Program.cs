ba27af1b51f21740ff2174f9032e68440248f15e
Your Name <you@example.com>
2022-08-02 23:41:25 +0530

    [Megha] Add: Adding Method

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NLogDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Sum sum = new Sum();
+            sum.DoSum(4, 9);
+        }
+    }
+}
