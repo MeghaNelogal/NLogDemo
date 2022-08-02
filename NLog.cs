981cf3d791cc9970a62190945fe6d49236f8aec5
Your Name <you@example.com>
2022-08-02 23:38:58 +0530

    [Megha] Add: Adding nlog.cs

---
+++ NLog.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo
+{
+    public class NLog
+    {
+        public static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebugMode(string message)
+        {
+            logger.Debug(message);
+        }
+
+        public void LogErroring(string message)
+        {
+            logger.Error(message);
+        }
+
+        public void LogInformation(string message)
+        {
+            logger.Info(message);
+        }
+
+
+
+
+    }
+}
